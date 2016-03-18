6076ff4a61d4ae9f42cdc3d322120e8560b6736a
Cameron Bergoon <cameron.bergoon@odwlogistics.com>
2016-03-18 10:38:49 -0400

    Adding adjacency matrix test class stub.

---
+++ java-graph/src/test/java/com/cbergoon/graph/AdjacencyMatrixGraphTest.java
@@ -0,0 +1,31 @@
+package com.cbergoon.graph;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AdjacencyMatrixGraphTest extends TestCase {
+
+    /**
+     * Create the test case
+     * @param testName name of the test case
+     */
+    public AdjacencyMatrixGraphTest(String testName){
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite(){
+        return new TestSuite(AdjacencyMatrixGraphTest.class);
+    }
+    
+    /**
+     * Test Graph Create
+     */
+    public void testCreate(){
+    	
+    }
+    
+}
