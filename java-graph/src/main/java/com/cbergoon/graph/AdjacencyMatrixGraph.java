8b5030fb7e8d0cc8a36a4625f7bfd9d4c36bb1d9
Cameron Bergoon <cameron.bergoon@odwlogistics.com>
2016-03-18 10:36:41 -0400

    Adding matrix data struct with sizing methods.

--- java-graph/src/main/java/com/cbergoon/graph/AdjacencyMatrixGraph.java
+++ java-graph/src/main/java/com/cbergoon/graph/AdjacencyMatrixGraph.java
@@ -6,6 +6,52 @@ import com.cbergoon.exceptions.InvalidVertexIndexException;
 
 public class AdjacencyMatrixGraph extends Graph {
 	
+	/**
+	 * Adjacency matrix data structure.
+	 */
+	private int[][] adjMatrix;
+	
+	/**
+	 * Default constructor for the adjacency matrix class. 
+	 */
+	public AdjacencyMatrixGraph(){
+		super();
+		initAdjMatrix(50);
+	}
+	
+	/**
+	 * Constructor with size argument for initial size of the matrix.
+	 * @param size Size of the adjacency matrix. 
+	 */
+	public AdjacencyMatrixGraph(int size){
+		super();
+		initAdjMatrix(size);
+	}
+	
+	/**
+	 * Initializes the adjacency matrix with all elements of value
+	 * 0. The size of the matrix will be equal on both sides as 
+	 * required by the graph. 
+	 * @param size The size of the array; both sides. 
+	 */
+	private void initAdjMatrix(int size){
+		adjMatrix = new int[size][size];
+	}
+	
+	/**
+	 * Increases the size of the adjacency matrix structure.
+	 * @param size Size of the new matrix;
+	 */
+	private void increaseAdjMatrixSize(int size){
+		int[][] newAdjMatrix = new int[size][size];
+		for(int i = 0; i < adjMatrix.length; i++){
+			for(int j = 0; j < adjMatrix[i].length; j++){
+				newAdjMatrix[i][j] = adjMatrix[i][j];
+			}	
+		}
+		adjMatrix = newAdjMatrix;
+	}
+	
 	@Override
 	public void addVertexImplementation() {
 		// TODO Auto-generated method stub
